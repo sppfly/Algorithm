01397f36d34bc12d2037f8291aaa7d5d7ec38508
liuyi <liuyicn@intsig.net>
2022-06-29 15:59:07 +0800

    debug

---
+++ src/main/java/princeton/PQ/MaxPQ.java
@@ -0,0 +1,37 @@
+package princeton.PQ;
+
+
+public class MaxPQ {
+    MaxPQ(int[] a) {
+
+    }
+
+
+
+
+    private void swim() {
+
+    }
+
+    void insert(int key) {
+
+    }
+
+
+    int max() {
+
+    }
+
+    int delMax() {
+
+    }
+
+
+    boolean isEmpty() {
+
+    }
+
+    int size() {
+
+    }
+}
--- src/main/java/princeton/UnionFind.java
+++ src/main/java/princeton/UnionFind.java
@@ -5,38 +5,42 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.UF;
 
 /**
-* @author shuxiahua
-* @Description:
-* @date 2021/4/20 21:54
-*/
+ * @author shuxiahua
+ * @Description:
+ * @date 2021/4/20 21:54
+ */
 public class UnionFind {
 
-   private int size;
+    private int size;
 
-   public UnionFind(int size) {
-       this.size = size;
-   }
+    public UnionFind(int size) {
+        this.size = size;
+    }
 
-   void union(int p, int q) {
+    void union(int p, int q) {
 
-   }
+    }
 
-   boolean connected(int p, int q) {
-       return false;
-   }
+    boolean connected(int p, int q) {
+        return false;
+    }
 
+    int count() {
+        return 0;
+    }
 
-   public static void main(String[] args) {
-       int n = StdIn.readInt();
-       UF uf = new UF(n);
-       while (!StdIn.isEmpty()) {
-           int p = StdIn.readInt();
-           int q = StdIn.readInt();
-           if (uf.connected(p, q)) continue;
-           uf.union(p, q);
-           StdOut.println(p + " " + q);
-       }
-       StdOut.println(uf.count() + " components");
-   }
+    public static void main(String[] args) {
+        int n = StdIn.readInt();
+        UnionFind uf = new UnionFind(n);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.connected(p, q))
+                continue;
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
+        StdOut.println(uf.count() + " components");
+    }
 
 }
