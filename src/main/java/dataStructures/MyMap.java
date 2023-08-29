3b9fc6761ade203c5ca83441c37cd78fa5f514f5
liuyi <liuyicn6@qq.com>
2023-08-29 19:26:33 +0200

    change the structure

--- src/main/java/dataStructures/MyMap.java
+++
@@ -1,9 +0,0 @@
-package dataStructures;
-
-/**
- * @author shuxiahua
- * @Description:
- * @date 2021/3/22 20:51
- */
-public interface MyMap {
-}
--- src/main/java/utils/Utils.java
+++
@@ -1,9 +0,0 @@
-package utils;
-public class Utils {
-
-    public static void swap(int nums[], int i, int j) {
-        int c = nums[i];
-        nums[i] = nums[j];
-        nums[j] = c;
-    }
-}
